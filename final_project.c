ab8e2c95ae51e395263a7620a4ac1c5cb5b6cd54
Yeng Miller-Chang <ymchang@iastate.edu>
2019-04-20 20:43:55 +0000

    Fixed input and added error checking.

--- final_project.c
+++ final_project.c
@@ -1,17 +1,74 @@
 #include "final_project.h"
 
 //compile as
-//gcc -o final_project -pedantic -Wall -lm
-//run as ./final_project [[a_11 a_12][a_21 a22]]
+//gcc -o final_project final_project.c -pedantic -Wall -lm
+//run as ./final_project a b c d -alpha e, where a, b, c, and d are integers, and e is a float
+
 int main(int argc, char *argv[])
 {
-	// input: a 2 x 2 contingency table
-	double **table;
+	// if the number of parameters is less than 5 (i.e., less than just the file name of the .exe and 4 numbers),
+	// return error
+	if (argc < 5 || argc == 6 || argc > 7) {
+		printf("Please input a 2-by-2 matrix of counts in the form a b c d in row-major order, \n");
+		printf("with optional switch -alpha e, e a number between 0 and 1 exclusive.\n");
+		exit(1);
+	}
 
-	/* read dataset*/
-	data = (double **) calloc(n, sizeof(double *));
+	if (strcmp(argv[5], "-alpha") != 0) {
+		printf("The switch you used is incorrect.\n");
+		printf("Use the correct switch -alpha e, with e a number between 0 and 1 exclusive.\n");
+		exit(1);
+	}
 
-	FREE_2ARRAY(data);
-	data = NULL;
+	// alpha and level for confidence interval
+	float alpha, level;
+	// input: four components of a 2 x 2 contingency table, in row-major order
+	int a, b, c, d;
+
+	// Use alpha = 5% by default
+	if (argc == 7) {
+		alpha = atof(argv[6]);	
+	} else {
+		alpha = 0.05;
+	}
+
+	// check for alpha in (0, 1)
+	if (!(alpha > 0 && alpha < 1)) {
+		printf("Please input a value of alpha between 0 and 1 exclusive.\n");
+		exit(1);
+	}
+
+	level = 1 - alpha; 
+
+	if (!stringNumericCheck(argv[1]) ||
+		!stringNumericCheck(argv[2]) ||
+			!stringNumericCheck(argv[3]) || 
+				!stringNumericCheck(argv[4])) {
+		printf("Please use numeric inputs for the four matrix components.\n");
+		exit(1);
+
+	};
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = atoi(argv[3]);
+	d = atoi(argv[4]);
+
+	if (a < 0 || b < 0 || c < 0 || d < 0) {
+		printf("The integer inputs must all be positive.\n");
+		exit(1);
+	}
+
 	return 0;
+}
+
+int stringNumericCheck (char *str) {
+	int i = 0;
+	int notNumericCount = 0;
+
+	for (i = 0; i < strlen(str); i++) {
+		notNumericCount += !(isdigit(str[i]));
+	}	
+
+	return !(notNumericCount > 0);
 }
--- final_project.h
+++ final_project.h
@@ -3,6 +3,24 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/* ---------- 1D arrays ---------------------- */
+
+#define MAKE_1ARRAY_DEFAULT(a, n) do {                                       \
+	(a) = malloc((n) * sizeof *(a));                                     \
+	if ((a) == NULL)                                                     \
+	PRINT("*** in file %s, function %s(), line %d: "                     \
+	"out of memory!\n",  __FILE__, __func__, __LINE__);                  \
+} while (0)
+
+#define FREE_1ARRAY(a)  do {                                                 \
+	free(a);                                                             \
+	a = NULL;                                                            \
+} while (0)
+
+
 
 #define MAKE_2ARRAY(a, m, n) do {                                            \
 	size_t ARRAY_H2RESERVED;                                             \
@@ -29,5 +47,6 @@
 	FREE_1ARRAY(a);                                                               \
 } while (0)
 
+int stringNumericCheck (char *str);
 
 #endif 
