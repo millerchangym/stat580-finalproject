0ae361326347a00294fc267ae8f7faab961c6f42
Yeng Miller-Chang <ymchang@iastate.edu>
2019-04-20 23:45:55 +0000

    Added binomial coefficient function

--- functions.c
+++ functions.c
@@ -1,6 +1,6 @@
 #include "final_project.h"
 
-int stringNumericCheck (char *str) {
+int stringNumericCheck(char *str) {
         int i = 0;
         int notNumericCount = 0;
 
@@ -9,4 +9,12 @@ int stringNumericCheck (char *str) {
         }
 
         return !(notNumericCount > 0);
+}
+
+// Recursive formula: https://en.wikipedia.org/wiki/Binomial_coefficient#Recursive_formula
+int binom(int n, int k) {
+	if (n == k || k == 0) {
+		return 1;
+	}
+	return binom(n-1, k-1) + binom(n-1, k);
 }
