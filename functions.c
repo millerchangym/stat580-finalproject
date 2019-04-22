deccdb8aa02ae3f2eda8b6e553db313a4c268425
Yeng Miller-Chang <ymchang@iastate.edu>
2019-04-22 22:08:11 +0000

    Drafted newton-raphson function

--- functions.c
+++ functions.c
@@ -93,3 +93,38 @@ double generate_poly_deriv(double theta, double alpha,
 	return out;
 }
 
+// implement Newton-Raphson
+double newton_raphson(function f, function f_prime, 
+			double theta_0,
+				double alpha,
+					unsigned long **numer, int numer_rows,
+						unsigned long **denom, int denom_rows) {
+
+	double threshold, theta_next, theta_prev;
+	int n_iter, n_iter_MAX;
+	int stop;
+
+	threshold = 0.001;
+	n_iter = 0;
+	n_iter_MAX = 5000;
+	theta_prev = theta_0;
+
+	do {
+		n_iter++;
+		print("Iteration %d\n: ", n_iter);
+		theta_next = theta_prev -
+			f(theta_prev, alpha, numer, numer_rows,
+				denom, denom_rows) /
+			f_prime(theta_prev, alpha, numer, numer_rows,
+				denom, denom_rows);
+		printf("New value: %f\n", theta_next);
+		// fabs is the floating-point absolute value
+		stop = (fabs(theta_next - theta_prev) <= threshold ||
+				n_iter >= n_iter_MAX);
+		if (!stop)
+			theta_prev = theta_next;
+		else
+			printf("STOPPING...\n");
+	} while (!stop)
+	return theta_next;
+}
